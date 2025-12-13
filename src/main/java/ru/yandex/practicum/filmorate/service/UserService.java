5c80354a9734ccc4b11d4e00e26e0238531e1401
Vanek_Vyaznikov <ivn.vznkv@yandex.ru>
2025-12-13 12:57:13 +0300

    правки чекстайл

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -10,7 +10,6 @@ import ru.yandex.practicum.filmorate.validation.FriendValidator;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
